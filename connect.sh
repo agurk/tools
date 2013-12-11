a6a4a0b7a96da18de5e3f91332a7b68830f2622c
Agurk <timothy.moll@gmail.com>
2013-12-11 17:56:13 +0200

    Script to store SSH connections in, with auth files if  needed

---
+++ connect.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/bash
+
+AUTH_FILE="/path/to/file"
+
+
+declare -A SERVERS
+#SERVERS[EXAMPLE]=user@server.com
+
+declare -A AUTHFILES
+#AUTHFILES[EXAMPLE]=AUTH_FILE
+
+ENV=$1
+
+old_ps1=$PS1
+ps1="\[\e]0;$ENV\a\]\n\[\e[32m\]\u@\h \[\e[33m\]\w\[\e[0m\]\n\$ "
+
+function usage
+{
+    cat<<EOF
+usage: connect.sh <env-name>
+
+EOF
+    echo Configured environments are:
+    paste <(
+    for k in "${!SERVERS[@]}"
+    do
+	echo "$k"
+    done
+    ) <(
+    for k in "${!SERVERS[@]}"
+    do
+        echo "${SERVERS[$k]}"
+    done
+
+    )
+}
+
+if [[ -z $ENV ]]
+    then
+	usage;
+	exit 1
+elif [[ ${SERVERS[$ENV]} ]]
+    then
+	if [[ ${AUTHFILES[$ENV]} ]]
+	    then
+		echo $PS1
+		echo $ps1
+		export PS1=$ps1
+		echo $PS1
+		echo ssh -i "${AUTHFILES[$ENV]}" ${SERVERS[$ENV]}
+		ssh -i "${AUTHFILES[$ENV]}" ${SERVERS[$ENV]}
+	else
+		PS1=$ps1
+		echo ssh ${SERVERS[$ENV]}
+		ssh ${SERVERS[$ENV]}
+	fi
+else
+    echo Unknown environment: $ENV
+    usage
+fi
+
